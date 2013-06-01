d59f5dae73b2565f0cf5da77f4a1f87da250b9b6
Rahul Nadella <rahulnadella@yahoo.com>
2013-06-01 16:02:35 -0400

    Initial Commit

---
+++ Assert/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Assert
+//
+//  Created by Rahul Nadella on 6/1/13.
+//  Copyright (c) 2013 Rahul Nadella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AssertTests/AssertTests.h
@@ -0,0 +1,13 @@
+//
+//  AssertTests.h
+//  AssertTests
+//
+//  Created by Rahul Nadella on 6/1/13.
+//  Copyright (c) 2013 Rahul Nadella. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AssertTests : SenTestCase
+
+@end
